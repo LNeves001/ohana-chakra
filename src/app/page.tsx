d2f155630c16142c5d3de8e522c24bf2e8bb4314
Lucas Martins <lmns01@hotmail.com>
2023-11-03 16:52:45 -0300

    ThemeConfigs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
-import { Box } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <Box bg='tomato' w='100%' p={4} color='white'>
-    This is the Box
-  </Box>
+  <h1>teste</h1>
   )
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,36 @@
 import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
-export const theme = extendTheme();
+export const theme = extendTheme({
+  fonts: {
+    body: "Inter, sans-serif",
+  },
+
+  boxSizing: {
+    boxSizing: "border-box",
+  },
+
+  colors: {
+    white: {
+      "500": "#FFFFFF",
+    },
+    gray: {
+      "100": "#F2F2F2",
+      "200": "#D9D9D9",
+      "300": "#808080",
+      "400": "#333333",
+      "500": "#262626",
+      "600": "#1A1A1A",
+      "700": "#0D0D0D",
+    },
+    yellow: {
+      "500": "#ECB62A",
+    },
+    orange: {
+      Light: "#D57B5A",
+    },
+    pink: {
+      "25": "#BF477E",
+      Dark: "#B22D95",
+    },
+  },
+});
